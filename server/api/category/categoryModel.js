f78ec19d7d70588ebfb7334a05a7a5ffaa47abd8
Gabriel Faucher <gpfaucher@gmail.com>
2018-01-27 16:11:25 +0100

    completed step 8

--- server/api/category/categoryModel.js
+++ server/api/category/categoryModel.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CategorySchema = new Schema({
-
+    name: String
 });
 
 module.exports = mongoose.model('category', CategorySchema);
